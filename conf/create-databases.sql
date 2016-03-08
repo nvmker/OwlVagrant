7b46826d9d0ba63889686d603c121715ee94e25d
mars <martin@rebel-it.com>
2016-03-08 15:18:58 +0000

    create mediawiki db

--- conf/create-databases.sql
+++ conf/create-databases.sql
@@ -1,3 +1,7 @@
-create database owl_staging_wp;
+create database if not exists owl_staging_wp;
 grant all privileges on owl_staging_wp.* to owl@localhost identified by 'owl' with grant option;
 grant all privileges on owl_staging_wp.* to owl@"%" identified by 'owl' with grant option;
+
+create database if not exists owl_staging_mediawiki;
+grant all privileges on owl_staging_mediawiki.* to owl_mediawiki@localhost identified by 'secret' with grant option;
+grant all privileges on owl_staging_mediawiki.* to owl_mediawiki@"%" identified by 'secret' with grant option;
